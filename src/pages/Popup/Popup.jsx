8d0411d2c5d33eb0f156153bdc85a4115832a16c
LukeSutor <lukesutor@gmail.com>
2021-06-06 12:35:07 -0400

    Design changes

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+import logo from '../../assets/img/icon-34.png'
+import svg from './settings.svg'
+import axios from 'axios';
 import './Popup.css';
 
 const Popup = () => {
@@ -39,37 +41,15 @@ const Popup = () => {
   }, [])
 
 
-  // If the question changes (which means the search button was pressed)
-  // Make an axios call to the hugging face api and send the question and context
-  // Then set the answer to the response
-  useEffect(() => {
-    if (question !== "") {
-      setLoading(true)
-      setAnswer("")
-      axios({
-        method: 'post',
-        url: 'https://api-inference.huggingface.co/models/distilbert-base-uncased-distilled-squad',
-        headers: { "Authorization": "Bearer api_zptRKxCtFJYHzwQraLnzCvXeOmRbLYLXNk" },
-        data: {
-          "inputs": {
-            "question": question,
-            "context": page == 0 ? pageText : document.getElementById("textarea").value
-          }
-        }
-      })
-        .then(res => setAnswer(res.data.answer))
-
-      setLoading(false)
-      if (answer == "") {
-        setAnswer("Sorry, no answer found")
-      }
-    }
-  }, [question])
-
+  // If the search button was pressed make an axios call to the hugging 
+  // face api and send the question and context then set the answer to the response
+  //
+  // After the call, if the answer is still an empty string, set it to "Sorry, no answer found"
+  // because that means an answer wasn't found.
   function handleSubmit() {
     setQuestion(page == 0 ? document.getElementById("search0").value : document.getElementById("search1").value)
 
-    if(question == "") {
+    if (question == "") {
       document.getElementById(page == 0 ? "search0" : "search1").placeholder = "Enter question"
       return
     } else {
@@ -99,6 +79,12 @@ const Popup = () => {
 
   return (
     <>
+      <div className="header">
+        <img src={logo} alt="" className="logo" />
+        <a className="settings-button">
+          <img src={svg} alt="svg" className="settings-svg" />
+        </a>
+      </div>
       <div className="button-container">
         <button className={`button ${page === 0 ? "button-active" : "button-inactive"}`} onClick={() => setPage(0)}>Scan Webpage</button>
         <button className={`button ${page === 1 ? "button-active" : "button-inactive"}`} onClick={() => setPage(1)}>Manually Enter</button>
@@ -109,7 +95,6 @@ const Popup = () => {
             <form className="form">
               <label for="search0">Type Question</label>
               <input type="text" id="search0" className="search" />
-              {/* () => setQuestion(document.getElementById("search0").value) */}
               <button type="button" className="submit"
                 onClick={() => handleSubmit()}>{loading ? "Loading..." : "Search"}</button>
             </form>
@@ -128,7 +113,7 @@ const Popup = () => {
               <label for="textarea">Paste Text To Scan</label>
               <textarea id="textarea" className="textarea" rows={4} />
               <button type="button" className="submit"
-                onClick={() => setQuestion(document.getElementById("search1").value)}>{loading ? "Loading..." : "Search"}</button>
+                onClick={() => handleSubmit()}>{loading ? "Loading..." : "Search"}</button>
             </form>
             {answer !== "" &&
               <>
