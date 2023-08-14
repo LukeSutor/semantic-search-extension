35afd1d71e88591d179879849fc92255a4944811
Luke Sutor <lukesutor@gmail.com>
2023-08-14 15:57:39 -0400

    make extension work with google docs

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -1,7 +1,23 @@
 chrome.runtime.onMessage.addListener(
     function (message, sender, sendResponse) {
       if(message.type == "getText") {
+        var url = window.location.href;
         var text = document.body.innerText;
+        // Check if it's google docs to properly fetch document data
+        if (url.startsWith("https://docs.google.com")) {
+          var scripts = document.getElementsByTagName("script")
+          for(var i = 0; i < scripts.length; i++) {
+            if (scripts[i].innerText.startsWith("DOCS_modelChunk")) {
+              var snippet = scripts[i].innerText;
+              var start = snippet.indexOf("[{");
+              var end = snippet.lastIndexOf("}]")
+              if(start != -1 && end != -1) {
+                var json = JSON.parse(snippet.substring(start, end + 2))
+                text += json[0].s;
+              }
+            }
+          }
+        }
         sendResponse(text);
       } else if(message.type == "highlight") {
         // ******** 
